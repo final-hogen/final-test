513151daf0b81fcb6cb579255d5306a731c3c240
final-hogen <hoggenn@gmail.com>
2021-06-16 12:29:36 +0900

    ボイステスト

---
+++ finalvoice/finalvoice.js
@@ -0,0 +1,69 @@
+
+// ajaxしょり
+class FinalVoice {
+  constructor() {
+    this.langScope = 'ja-JP';
+    this.useOK = !!window.speechSynthesis;
+    this.enableVoices = {}
+    this.resetEnableVoices();
+    var that = this;
+    window.speechSynthesis.onvoiceschanged = function(){
+      that.resetEnableVoices();
+    }
+    this.voices = {};
+  }
+  /**
+   * 使えるボイスのリセット
+   */
+  resetEnableVoices() {
+    if(!this.useOK)return;
+    var voices = window.speechSynthesis.getVoices();
+    for(var i = 0; i < voices.length; i++) {
+      var voice = voices[i];
+      if(this.langScope && voice.lang!=this.langScope)continue;
+      this.enableVoices[voice.name] = voice;
+    }
+  }
+  makeVoiceData(setVoice,setPitch,setRate){
+    return {voice:setVoice,pitch:setPitch,rate:setRate};
+  }
+  /**
+   * ランダムで不使用ボイスを取得
+   */
+  getRandomVoice(){
+    var usedKeys = [];
+    Object.values(this.voices).forEach(voice=>usedKeys.push(voice.voice.name));
+    var enableKeys = Object.keys(this.enableVoices);
+    var voices = enableKeys.filter(name => !usedKeys.includes(name) );
+    var voiceIndex = Math.floor(Math.random() * voices.length);
+    var useVoice = this.enableVoices[voices[voiceIndex]];
+    var pitch = 1.0+(Math.random()*0.8);
+    var rate = 1.0+(Math.random()*0.2);
+    return this.makeVoiceData(useVoice,pitch,rate);
+  }
+  /**
+   * 声取得(ない時ランダム設定))
+   * @param {キャラ名} name 
+   */
+  getVoice(name){
+    if(this.voices[name])return this.voices[name];
+    var newVoice = this.getRandomVoice();
+    this.voices[name] = newVoice;
+    return newVoice;
+  }
+  /**
+   * 話す
+   * @param {キャラ名} name 
+   * @param {セリフ} text 
+   */
+  speak(name, text){
+    text = text.replace(/(<([^>]+)>)/gi, '');
+    window.speechSynthesis.cancel();
+    var speech = new SpeechSynthesisUtterance(text);
+    var voice = this.getVoice(name);
+    speech.voice = voice.voice;
+    speech.pitch = voice.pitch;
+    speech.rate = voice.rate;
+    window.speechSynthesis.speak(speech);
+  }
+}

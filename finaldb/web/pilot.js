6e885585d834d1a9dd9c83ba3b17394de9b9c1a0
final-hogen <hoggenn@gmail.com>
2023-02-18 22:02:19 +0900

    途中まで

--- finaldb/web/pilot.js
+++ finaldb/web/pilot.js
@@ -1,19 +1,26 @@
 
 class FinalPilotData{
-  static getNickname(baseNode){
+  static getNickname(baseNode,excludeName=null){
     var result = {};
     for(var name in baseNode){
+      if(name==excludeName)continue;
       result[name] = name;
     }
-    result["カーティス"] = "カーティス・ベルナル";
-    result["コディス"] = "カーティス・ベルナル";
-    result["タイシア"] = "タイシア・グラフト";
-    result["シャルン"] = "シャロン";
-    result["ザンテ"] = "ジェンシ";
-    result["ストレンジャー"] = "ザ・ストレンジャー";
-    result["変異体イブリン"] = "イブリン（変異体）";
-    result["ミラベル"] = "ミラベル・デコリー";
-    result["モルガン"] = "モルガン・ヘリング";
+    const nickNames = [
+      ["カーティス","カーティス・ベルナル"],
+      ["コディス","カーティス・ベルナル"],
+      ["タイシア","タイシア・グラフト"],
+      ["シャルン","シャロン"],
+      ["ザンテ","ジェンシ"],
+      ["ストレンジャー","ザ・ストレンジャー"],
+      ["変異体イブリン","イブリン（変異体）"],
+      ["ミラベル","ミラベル・デコリー"],
+      ["モルガン","モルガン・ヘリング"],
+    ];
+    for( var keyval of nickNames){
+      if(keyval[1]==excludeName)continue;
+      result[keyval[0]] = keyval[1];
+    }
     return result;
   }
 }
@@ -136,5 +143,91 @@ class PilotInStory extends FinalJsonDB
     var splitPilot = pilots.split(",");
     var result = {"タイトル":title,"パイロット":splitPilot,"ファイル":file};
     this.storys[title] = result;
+  }
+}
+
+class StatusCircle
+{
+  static polygonScale = [  // 0から100 xy
+    [0,-50],
+    [47.5528258147577,-15.4508497187474],
+    [29.3892626146237,40.4508497187474],
+    [-29.3892626146237,40.4508497187474],
+    [-47.5528258147577,-15.4508497187474],
+  ];
+  static centerPoint = 50;
+  constructor(node){
+    var SSRcount = 0;
+    var Max = {"耐久":0,"攻撃":0,"防御":0,"回避":0,"会心":0};
+    var SSRSum = {"耐久":0,"攻撃":0,"防御":0,"回避":0,"会心":0};
+    for(let i in node){
+      let pilot = node[i];
+      let staus = pilot["ステータス"]["ステ合計"];
+      let isSSR = pilot["レアリティ"]=="SSR";
+      for(let key in Max){
+        Max[key] = Math.max(Max[key],staus[key]);
+      }
+      if(isSSR){
+        for(let key in SSRSum){
+          SSRSum[key] += parseInt(staus[key]);
+        }
+        ++SSRcount;
+      }
+    }
+    for(let key in SSRSum){
+      SSRSum[key] /= SSRcount;
+    }
+    this.maxStatus = Max;
+    this.aveStatus = SSRSum;
+  }
+  getPascentArray(status){
+    return [
+      (status["耐久"])/(this.maxStatus["耐久"]),
+      (status["攻撃"])/(this.maxStatus["攻撃"]),
+      (status["防御"])/(this.maxStatus["防御"]),
+      (status["回避"])/(this.maxStatus["回避"]),
+      (status["会心"])/(this.maxStatus["会心"]),
+    ];
+  }
+  getPilotObjectCSS(piotObject){
+    let array = this.getPascentArray(piotObject.fetchData("ステータス/ステ合計"));
+    return StatusCircle.joinCSS(StatusCircle.makePolygon(array));
+  }
+  getPilotObjectPolygon(piotObject){
+    let array = this.getPascentArray(piotObject.fetchData("ステータス/ステ合計"));
+    return StatusCircle.joinSVG(StatusCircle.makePolygon(array));
+  }
+  getAvePolygon(){
+    return StatusCircle.joinSVG(StatusCircle.makePolygon(this.getPascentArray(this.aveStatus)));
+  }
+  getMaxPolygon(){
+    return StatusCircle.joinSVG(makePolygon([1,1,1,1,1]));
+  }
+  static makePolygon(pacentArray){
+    var result = [];
+    for(let i in pacentArray){
+      let s = [StatusCircle.polygonScale[i][0] * pacentArray[i]+this.centerPoint
+        ,StatusCircle.polygonScale[i][1] * pacentArray[i]+this.centerPoint];
+        result.push(s);
+    }
+    return result;
+  }
+  static joinCSS(polygon){
+    var join = [];
+    for(let i in polygon){
+      let xy = polygon[i];
+      var xystr = xy[0]+"% "+xy[1]+"%";
+      join.push(xystr);
+    }
+    return "clip-path: polygon("+join.join(",")+");";
+  }
+  static joinSVG(polygon){
+    var join = [];
+    for(let i in polygon){
+      let xy = polygon[i];
+      var xystr = xy[0]+","+xy[1];
+      join.push(xystr);
+    }
+    return join.join(" ");
   }
 }
